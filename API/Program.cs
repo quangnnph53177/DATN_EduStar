91b66f4a0ecc255e969721da3bac913bfdc59051
Đạt Nguyễn <manhdat2890@gmail.com>
2025-05-27 10:14:20 +0700

    Revert "#datlam"
    This reverts commit 82bdd2cb47991ab693fd871b4d7de59fcf763127.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,5 @@
 using API.Data;
-using API.Services;
-using API.Services.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IStudent, StudentsRepos>();
-builder.Services.AddScoped<IRegisterService, RegisterRepos>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
