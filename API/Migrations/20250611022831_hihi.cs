bc2bb8b98b36acda768b85f3a38aa5be18deb8fe
huyntrang9105 <phamhuyentrang09102005@gmail.com>
2025-06-12 16:41:42 +0700

    hi

---
+++ API/Migrations/20250611022831_hihi.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class hihi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClassCode",
+                table: "StudentsInfor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClassCode",
+                table: "StudentsInfor",
+                type: "nvarchar(90)",
+                maxLength: 90,
+                nullable: true);
+        }
+    }
+}
